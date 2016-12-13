d9802498e3e7c259c085e6e566bc01eba6065600
Alexander Hofmann <alexander.hofmann@technikum-wien.at>
2016-12-13 20:26:58 +0100

    ready and working first version

---
+++ mz_display.ino
@@ -0,0 +1,173 @@
+/************************************************************
+ mz_display code                   
+ V1.0.0                          
+ (c) 2016 hofmann engineering    
+                                 
+ So this code will run on an Adafruit 
+ HUZZAH ESP8266 Board. To program the board do:
+   (1) Hold down the GPIO0 button, the red LED will be lit
+   (2) While holding down GPIO0, click the RESET button
+   (3) Release RESET, then release GPIO0
+   (4) When you release the RESET button, the red LED will 
+      be lit dimly, this means its ready to bootload
+ Upload Speed is 115200 baud.
+
+ This program puts the watt usage of our house, coming from a
+ mqtt server on a sh1106 display.
+*************************************************************/  
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <stdlib.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+
+//#define DEBUG 1                           //Use this in debugging environment only  
+
+#define WIFI_SSID "HWLAN"                   //WIFI SSID of your NETWORK
+#define WIFI_PASSWORD "1234567890123456"    //WIFI Password of your NETWORK
+
+#define SERIAL_BAUD_RATE  115200
+#define MQTT_SERVER_IP "192.168.178.2"         //MQTT Server Address within your NETWORT
+#define MQTT_PORT 1883                         //MQTT standard port is 1883
+#define MQTT_CLIENT_NAME  "mqttdisplay"        //MQTT client name - here default is rxtx2mqtt01   
+
+void setup_wifi();    //forward declaration to function setup_wifi();
+
+WiFiClient espClient;                       //WiFiClient Object, needed to connect
+PubSubClient client(espClient);             //Connection to MQTT Server
+
+float watt = 0.0f;
+char watt_s[7];
+
+//display object - uses the u8g2 lib
+U8G2_SH1106_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, 12, 13);
+
+/************************************
+ function setup                  
+   initializes everything
+   (1) Serial connection
+   (2) Wifi
+   (3) MQTT Server connection
+   (4) u8g2 lib for the display        
+ parameter none                  
+ return none                     
+************************************/
+void setup(void) {
+  Serial.begin(SERIAL_BAUD_RATE);   //initialize the Serial object with speed of SERIAL_BAUD_RATE
+  setup_wifi();                     //initialize the wifi connection
+  client.setServer(MQTT_SERVER_IP, MQTT_PORT);  //initialize the connection to the mqtt_server
+  u8g2.begin();
+}
+/************************************
+ function update                  
+   will be called on receive of a new
+   mqtt message.        
+ parameter 
+   char* topic contains the topic
+   byte* payload contains the message
+   unsigned int length of the message                  
+ return none                     
+************************************/
+void update(char* topic, byte* payload, unsigned int length) {
+  int i=0;
+  char message_buff[255];
+  for(i=0; i<length; i++) {
+    message_buff[i] = payload[i];
+  }
+  message_buff[i] = '\0';
+  watt = atof(message_buff);
+  #ifdef DEBUG
+    Serial.print(topic);
+    Serial.print("=");
+    Serial.print(message_buff);
+    Serial.print("\n");
+  #endif  
+  
+}
+/************************************
+ function setup_wifi                  
+   starts the WiFi object and connects        
+ parameter none                  
+ return none                     
+************************************/
+void setup_wifi() {
+  delay(10);
+  #ifdef DEBUG
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(WIFI_SSID);
+  #endif
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);     //connect to WiFi SSID and PASSWORD
+
+  while (WiFi.status() != WL_CONNECTED) {   //wait until connected
+    delay(500);
+    #ifdef DEBUG
+      Serial.print(".");
+    #endif
+  }
+  #ifdef DEBUG
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  #endif
+}
+/************************************
+ function reconnect                  
+   restarts the MQTT connection        
+ parameter none                  
+ return none                     
+************************************/
+void reconnect() {
+  while (!client.connected()) {                         //try until connected
+    #ifdef DEBUG
+      Serial.println("Attempting MQTT connection...");
+    #endif
+    if (client.connect(MQTT_CLIENT_NAME)) {                  //client should connect
+      #ifdef DEBUG
+        Serial.println("connected");
+      #endif
+      client.setCallback(update);
+      boolean rc = client.subscribe("home/techroom/mz/analog");   //subscribe the topic to follow
+      #ifdef DEBUG
+        if (rc) {
+           Serial.println("subscripted topic");      
+        }
+      #endif
+    } else {
+      #ifdef DEBUG
+        Serial.print("failed, rc=");
+        Serial.print(client.state());
+        Serial.println(" try again in 5 seconds");
+      #endif
+      // Wait 2 seconds before retrying
+      delay(2000);
+    }
+  }
+}
+
+/************************************
+ function loop                  
+   loop function does
+     (1) reconnect if connection lost
+     (2) convert readed watts to string 
+     (3) display watts on the display
+ parameter none                  
+ return none                     
+************************************/
+void loop(void) {
+  if (!client.connected()) {    //if connection is lost, reconnect
+    reconnect();
+  }
+  client.loop();                //mqtt loop
+  dtostrf(watt, 6, 0, watt_s);  //convert watts to string
+  u8g2.clearBuffer();					  // clear the internal memory
+  u8g2.setFont( u8g2_font_logisoso22_tf);	// choose a suitable font
+  u8g2.drawStr(0, 44, watt_s);  //draw watts as a string on display
+  u8g2.drawStr(100, 44, "W");   //and the unit behing
+  u8g2.sendBuffer();					  // transfer internal memory to the display
+  delay(500);                  //do this ever 500ms
+}
+
